f4198ba74a9b950d640253683de40da31da7613c
AKSHAT SINGH <149935882+akshatxq@users.noreply.github.com>
2024-11-09 19:51:20 +0530

    Update Sidebar.jsx

--- annonify-web/src/components/Sidebar.jsx
+++ annonify-web/src/components/Sidebar.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { PiChatsBold } from "react-icons/pi";
-import { IoLinkSharp } from "react-icons/io5";
+import { IoLinkSharp } from "react-icons/io5"; 
 import { MdOutlinePermMedia } from "react-icons/md";
+import { IoSettingsOutline } from "react-icons/io5";
 
 function Sidebar() {
   return (
@@ -12,27 +13,34 @@ function Sidebar() {
         </div>
         <div className="p-4 space-y-10">
           <nav className="flex flex-col items-center space-y-8">
-            <div className="flex flex-col items-center text-gray-200 hover:text-white cursor-pointer">
+            <div className="flex flex-col items-center text-blue-500 hover:text-white cursor-pointer">
               <PiChatsBold size={24} />
-              <span className="text-sm mt-2 font-roboto-mono">ALL CHATS</span> {/* Apply font */}
+              <span className="text-sm mt-2 font-roboto-mono">ALL CHATS</span>
             </div>
-            <div className="flex flex-col items-center text-gray-200 hover:text-white cursor-pointer">
+            <div className="flex flex-col items-center text-blue-500 hover:text-white cursor-pointer">
               <MdOutlinePermMedia size={24} />
-              <span className="text-sm mt-2 font-roboto-mono">MEDIA</span> {/* Apply font */}
+              <span className="text-sm mt-2 font-roboto-mono">MEDIA</span>
             </div>
-            <div className="flex flex-col items-center text-gray-200 hover:text-white cursor-pointer">
+            <div className="flex flex-col items-center text-blue-500 hover:text-white cursor-pointer">
               <IoLinkSharp size={24} />
-              <span className="text-sm mt-2 font-roboto-mono">LINKS</span> {/* Apply font */}
+              <span className="text-sm mt-2 font-roboto-mono">LINKS</span>
             </div>
           </nav>
         </div>
       </div>
-      <div className="p-6">
-        <div className="flex items-center justify-center">
+      <div className="p-6 flex flex-col items-center">
+        {/* Settings Icon and Text Below it */}
+        <div className="flex flex-col items-center mb-6"> {/* Increased bottom margin here */}
+          <IoSettingsOutline size={32} className="text-gray-300 hover:text-white cursor-pointer" />
+          <span className="text-sm mt-2 font-roboto-mono text-gray-300 hover:text-white">SETTINGS</span>
+        </div>
+
+        {/* User Avatar with increased size */}
+        <div>
           <img
-            src="https://via.placeholder.com/40"
+            src="https://via.placeholder.com/60" // Increased size for the avatar
             alt="User Avatar"
-            className="rounded-full w-8 h-8"
+            className="rounded-full w-13 h-13" // Increased width and height
           />
         </div>
       </div>
