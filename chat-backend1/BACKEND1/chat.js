a9b31e73a599925511911524c685a86889351ed0
Anshukumar123975 <anshupat2020@gmail.com>
2024-11-10 19:45:31 +0530

    Added room code for client and server

--- chat-backend1/BACKEND1/chat.js
+++ chat-backend1/BACKEND1/chat.js
@@ -10,10 +10,12 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
-      io.emit('chat message', msg);
-    });
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
   });
+});
+
+
 server.listen(3000, () => {
   console.log('listening on *:3000');
 });
