3ff7b6b10cb066dd9e8fb2a0d4b6c2d5a34b8856
Aman Jha <150509057+i-aman-jha@users.noreply.github.com>
2024-11-16 01:50:48 +0530

    created comment model

---
+++ annonify-app/lib/models/blog/comment_model.dart
@@ -0,0 +1,13 @@
+class Comment{
+  String content;
+  String postId;
+  DateTime createdAt;
+
+
+  Comment({
+    required this.content,
+    required this.postId,
+    required this.createdAt,
+
+  });
+}
